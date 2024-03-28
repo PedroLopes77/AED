cb4651b02b0c26bd6bbfc3b8469442bcced39af2
PedroLopes77 <164026844+PedroLopes77@users.noreply.github.com>
2024-03-27 21:10:39 -0300

    Create lista2_5.cpp

---
+++ POO/lista2_5.cpp
@@ -0,0 +1,3 @@
+//Herança e polimorfismo são conceitos essenciais da POO. A herança permite que você
+//compartilhe propriedades e métodos entre classes, promovendo a reutilização de código.
+//O polimorfismo permite que você trate objetos de classes diferentes de maneira uniforme, tornando seu código mais flexível e extensível.
