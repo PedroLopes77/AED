35fe71f4381b4aa87ee02796438229e6f603019f
PedroLopes77 <164026844+PedroLopes77@users.noreply.github.com>
2024-03-27 21:14:12 -0300

    Create lista2_6.cpp

---
+++ POO/lista2_6.cpp
@@ -0,0 +1,7 @@
+//O encapsulamento traz vários benefícios para a programação, tais como: 
+//Segurança: ao esconder os dados sensíveis ou críticos de um objeto, você evita que eles sejam acessados ou alterados indevidamente por outras partes do código,
+//o que poderia causar erros ou inconsistências.
+//private: significa que o elemento só pode ser acessado ou modificado pelo próprio objeto que o declara, e não por nenhum outro objeto ou classe.
+//public: significa que o elemento pode ser acessado ou modificado por qualquer objeto ou classe, sem restrições.
+//protected: significa que o elemento pode ser acessado ou modificado pelo próprio objeto que o declara, ou por qualquer objeto ou classe que seja
+//do mesmo pacote ou que seja uma subclasse do objeto que o declara.
