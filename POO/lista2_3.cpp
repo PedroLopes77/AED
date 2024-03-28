da275cfbebfb613513fd3197d4d5a488ac82af5c
PedroLopes77 <164026844+PedroLopes77@users.noreply.github.com>
2024-03-27 21:06:27 -0300

    Create lista2_3.cpp

---
+++ POO/lista2_3.cpp
@@ -0,0 +1,16 @@
+//Atributos são as propriedades de um objeto. Métodos são as ações que um objeto pode realizar. Naturalmente, os atributos de um objeto podem mudar de acordo com o tempo.
+//De fato, a execução de um método muda os valores dos atributos do objeto responsável pela execução.
+
+//Exemplo:
+
+ _________________________
+|        Pessoa           |
+|-------------------------|
+| - nome: string          |
+| - idade: int            |
+|-------------------------|
+| + setNome(novoNome: string) |
+| + setIdade(novaIdade: int) |
+| + aniversario()         |
+| + getInfo()             |
+|_________________________|
