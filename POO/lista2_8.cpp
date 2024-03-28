6889b2de43ff0fc055dea5f4c312791b73fa0969
PedroLopes77 <164026844+PedroLopes77@users.noreply.github.com>
2024-03-27 21:19:06 -0300

    Create lista2_8.cpp

---
+++ POO/lista2_8.cpp
@@ -0,0 +1,2 @@
+//Herança é um dos pontos chave de programação orientada a objetos (POO). 
+//Ela fornece meios de promover a extensibilidade do código, a reutilização e uma maior coerência lógica no modelo de implementação.
