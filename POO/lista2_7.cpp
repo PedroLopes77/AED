3a085d7ea20b4313436b8fdb99f06ba6fa9cdda1
PedroLopes77 <164026844+PedroLopes77@users.noreply.github.com>
2024-03-27 21:18:09 -0300

    Create lista2_7.cpp

---
+++ POO/lista2_7.cpp
@@ -0,0 +1,16 @@
+//Getters e setters permitem o controle sobre como um atributo é acessado ou modificado. Eles podem incluir lógica adicional, como validação,
+//ou podem ser usados para criar atributos somente leitura e é este o motivo de sua utilização independente da linguagem de programação que venha utiliza-lo.
+
+//Exemplo :
+
+ _________________________
+|        Pessoa           |
+|-------------------------|
+| - nome: string          |
+| - idade: int            |
+|-------------------------|
+| + getNome(): string     |
+| + setNome(novoNome: string): void |
+| + getIdade(): int       |
+| + setIdade(novaIdade: int): void |
+|_________________________|
